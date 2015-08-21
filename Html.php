b58f1fc3d725a8f46edfb724d987979f194449f6
unknown <mr.igor.prokofev@gmail.com>
2015-08-21 13:26:02 +0500

    Html helpers

---
+++ Html.php
@@ -0,0 +1,28 @@
+<?php
+namespace valiant\helpers;
+
+/**
+ * Class Html
+ * @package valiant\helpers
+ */
+class Html extends \yii\helpers\Html
+{
+
+	/**
+	 * Generates span glyph icon tag.
+	 * @param string $icon name. e.g. "add" for glyphicon-add.
+	 * @param array $options the tag options in terms of name-value pairs. These will be rendered as
+	 * the attributes of the resulting tag. The values will be HTML-encoded using [[encode()]].
+	 * If a value is null, the corresponding attribute will not be rendered.
+	 * See [[renderTagAttributes()]] for details on how attributes are being rendered.
+	 * @return string the generated glyph icon span tag.
+	 */
+	public static function icon($icon, $options = [])
+	{
+		static::addCssClass($options, [
+			'glyphicon',
+			sprintf('glyphicon-s', $icon),
+		]);
+		return static::tag('span', '', $options);
+	}
+}
